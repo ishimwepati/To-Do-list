001563e216d6bc859b72934d92ff0d4d21f34f6f
ishimwepati <wazacode@gmail.com>
2023-07-20 19:29:49 +0300

    All added

---
+++ src/modules/tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+    {
+      description: 'Buy groceries',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Clean the house',
+      completed: true,
+      index: 2,
+    },
+    {
+      description: 'Finish homework',
+      completed: false,
+      index: 3,
+    },
+    // Add more tasks here if needed
+  ];
+  export default tasks;
