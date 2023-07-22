cccae05046dcd5e58a88c93f8b70494748047c17
ishimwepati <wazacode@gmail.com>
2023-07-22 20:56:29 +0300

    all added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,72 @@
-import './style.css';
-import { addNewTask } from './modules/addRemove.js';
-import { renderTasks } from './modules/new.js';
-// import { editTaskDescription, saveEditedTaskDescription } from './modules/edit.js';
+import './index.scss';
 
-const newTaskInput = document.getElementById('newTaskInput');
+import viewTasks from './modules/ViewTask.js';
+import addNewTask from './modules/AddNewTask.js';
+import removeTask from './modules/RemoveTask.js';
+import editTask from './modules/EditTask.js';
+import taskStatusUpdate from './modules/TaskStatusUpdate.js';
+import clearCompletedTasks from './modules/ClearCompletedTasks.js';
 
-newTaskInput.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    const newTaskDescription = newTaskInput.value.trim();
-    if (newTaskDescription !== '') {
-      addNewTask(newTaskDescription);
-      newTaskInput.value = '';
-    }
+let tasks = JSON.parse(localStorage.getItem('toDoList')) || [];
+window.addEventListener('load', viewTasks(tasks));
+const input = document.querySelector('.input');
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter' && input.value !== '') {
+    const description = input.value;
+    tasks.push(addNewTask(description, tasks.length));
+    viewTasks(tasks);
+    input.value = '';
   }
 });
 
-renderTasks();
+document.addEventListener('click', (event) => {
+  // Remove task functionality
+  const deleteIcons = document.querySelectorAll('.delete-img');
+  deleteIcons.forEach((icon, index) => {
+    if (event.target === icon) {
+      removeTask(tasks, index);
+      viewTasks(tasks);
+    }
+  });
+
+  // Edit task functionality
+  const descriptions = document.querySelectorAll('.description');
+  descriptions.forEach((task, index) => {
+    if (event.target === task) {
+      const parentLi = event.target.parentNode;
+      parentLi.classList.add('edit-bg');
+      const oldTask = tasks[index].description;
+      const inputField = document.createElement('input');
+      inputField.type = 'text';
+      inputField.className = 'description edit-bg';
+      inputField.value = oldTask;
+      task.innerHTML = '';
+      task.appendChild(inputField);
+      inputField.focus();
+
+      inputField.addEventListener('blur', () => {
+        const newTask = inputField.value;
+        task.removeChild(inputField);
+        task.innerText = newTask;
+        editTask(tasks, index, newTask);
+        viewTasks(tasks);
+      });
+    }
+  });
+
+  // Checkbox status functionality
+  const checkBoxes = document.querySelectorAll('.check-box');
+  checkBoxes.forEach((checkBox, index) => {
+    checkBox.addEventListener('change', () => {
+      taskStatusUpdate(tasks, index);
+      viewTasks(tasks);
+    });
+  });
+
+  // Clear completed tasks functionality
+  const clearBtn = document.querySelector('.clear');
+  if (event.target === clearBtn) {
+    tasks = clearCompletedTasks(tasks);
+    viewTasks(tasks);
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
 };
